b07392f9defd2ebeefeefe14ad9ad30a0cbc6eb0
isro <mohammadisro2710@gmail.com>
2025-11-05 22:07:41 +0700

    feat: add new contract

--- contract/ignition/modules/FishItSetup.ts
+++ contract/ignition/modules/FishItSetup.ts
@@ -1,18 +1,9 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules"
+import FishItRootModule from "./FishItRoot.js"
 
-export default buildModule("FishItRootModule", (m) => {
-  const initialOwner = m.getParameter("initialOwner", m.getAccount(0))
-
-  const token = m.contract("FishItToken", [initialOwner])
-  const nft = m.contract("FishItNFT", [initialOwner])
-  const baitShop = m.contract("FishItBaitShop", [token, initialOwner])
-  const faucet = m.contract("FishItFaucet", [token, initialOwner])
-  const staking = m.contract("FishItStaking", [
-    token,
-    nft,
-    baitShop,
-    initialOwner,
-  ])
+export default buildModule("FishItSetupModule", (m) => {
+  const { token, nft, baitShop, faucet, staking } =
+    m.useModule(FishItRootModule)
 
   // Fund Faucet (300k FSHT)
   const faucetAmount = m.getParameter("faucetAmount", 300_000n * 10n ** 18n)
